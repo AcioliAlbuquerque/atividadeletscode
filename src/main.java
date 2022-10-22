502d793f537f31a3c1b62210a78291d49272cce8
Lucas Acioli Albuquerque <107963767+AcioliAlbuquerque@users.noreply.github.com>
2022-10-21 23:35:58 -0300

    v2

--- src/main.java
+++ src/main.java
@@ -1,37 +1,76 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-
 public class main {
-
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Qual é a quantidade de temperaturas para transformação que deseja: ");
-        int valor = teclado.nextInt();
-        System.out.println("Qual é o estado da temperatura que deseja: ");
-        System.out.println("1 - Celsius ");
-        System.out.println("2 - Kelvin");
-        System.out.println("3 - Fahrenheit");
 
-        System.out.println("Qual o estado de origem: ");
-        int temperaturaOrigem = teclado.nextInt();
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a quantidade de temperaturas");
+        int hh = teclado.nextInt();
+
+        int zz = 1;
+        List<Double> d = new ArrayList<>();
+
+        for (int i = 0; i < hh; i++) {
+            System.out.println("Temperatura " + zz++);
+            double kl = teclado.nextDouble();
+            d.add(kl);
+        }
 
         System.out.println("Qual o estado de destino: ");
+
+        System.out.println("1 - Kelvin -> Celsius ");
+        System.out.println("2 - Kelvin -> Fahrenheit ");
+        System.out.println("3 - Celsius -> Kelvin ");
+        System.out.println("4 - Celsius -> Fahrenheit ");
+        System.out.println("5 - Fahrenheit -> Celsius ");
+        System.out.println("6 - Fahrenheit -> Kelvin ");
         int temperaturaDestino = teclado.nextInt();
-        int resultado = 0;
 
-        System.out.println("Kelvin -> Celsius");
-        if (temperaturaOrigem == 1 && temperaturaDestino == 2) {
-            resultado = valor - 273;
-            System.out.println(resultado);
+        int z = 1;
+        double soma = 0;
+        for (int i = 0; i < d.size(); i++) {
+            if (temperaturaDestino == 1) {
+                double resul = d.get(i) - 273;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
 
-        } else if (temperaturaOrigem == 2 && temperaturaDestino == 3) {
-            resultado = ((int) (valor * 1.8)) + 32;
-            System.out.println(resultado);
+            } else if (temperaturaDestino == 2) {
+                double resul = (d.get(i) - 273.15) * 9 / 5 + 32;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
 
+            } else if (temperaturaDestino == 3) {
+                double resul = d.get(i) + 273.15;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
 
-        } else if (temperaturaOrigem == 3 && temperaturaDestino == 4) {
-            double fd = valor + 273.15;
-            System.out.println(fd);
+            } else if (temperaturaDestino == 4) {
+                double resul = (d.get(i) * 9 / 5) + 32;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
+
+            } else if (temperaturaDestino == 5) {
+                double resul = (d.get(i) - 32) * 5 / 9;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
+
+            } else if (temperaturaDestino == 6) {
+                double resul = (d.get(i) - 32) * 5 / 9 + 273.15;
+                soma += resul;
+                System.out.print(z++ + " Varlos: ");
+                System.out.printf("%.2f %n", resul);
+            }
         }
+        System.out.printf("Media dos valores : %.2f", soma / d.size());
     }
 }
+
+
+
